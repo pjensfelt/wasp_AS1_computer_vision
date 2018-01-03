107afb3a43d10bbb90285131459730afefc42207
Patric Jensfelt <patric@kth.se>
2018-01-03 22:13:38 +0100

    Added python version of the load and display notenook

---
+++ cv_01_loading_and_displaying.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2 as cv
+
+# The path to (relative to where jupyter is running) the image to load
+imageName = "test_images/lena.jpg"
+
+# Load the source image
+image = cv.imread(imageName)
+
+# We can comvert the image directly to gray scale if we want.
+# We do that??? Well, in many cases the algorithm we want to run is not using the color information.
+# cv2.IMREAD_COLOR (1)     : Loads a color image. Any transparency of image will be neglected. It is the default flag.
+# cv2.IMREAD_GRAYSCALE (0) : Loads image in grayscale mode
+# cv2.IMREAD_UNCHANGED (-1): Loads image as such including alpha channel
+imageG = cv.imread(imageName, cv.IMREAD_GRAYSCALE)
+
+print("We remind ourself about the fact that images are merely matrices with numbers")
+print(imageG)
+
+# Show the gray scale image
+cv.imshow('Gray scale image',imageG)
+
+# Show the color image
+cv.imshow('Color image', image)
+
+print("The first image window proably ended up below the second one")
+print("Wating for the user to press ENTER with one of the image windows active")
+cv.waitKey(0)
+cv.destroyAllWindows()
